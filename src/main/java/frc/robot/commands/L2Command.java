12fe96f7caa31f36197226be8c08dd42148b15d7
EllaphantF <Mariella.Favor@students.alemany.org>
2025-02-22 00:16:07 -0800

    Create L2Command.java

---
+++ src/main/java/frc/robot/commands/L2Command.java
@@ -0,0 +1,31 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.*;
+import frc.robot.subsystems.superstructure.SuperstructureSubsystem;
+
+public class L2Command extends Command {
+    SuperstructureSubsystem structure;
+
+    public L2Command(SuperstructureSubsystem structure) {
+        this.structure = structure;
+    }
+
+    @Override
+    public void initialize() {
+        if(structure.hasCoral = true){
+        structure.startLifting();
+        structure.liftL2();
+        }
+    }
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        structure.goHome();
+    }
+    
+    public boolean isFinished() {
+        return structure.scoringCoral = false;
+    }
+}
